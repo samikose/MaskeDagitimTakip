7caf05e4dae2f5f3b61b916091c6758c8fabed93
samikose <samikose1227@hotmail.com>
2022-12-14 03:19:41 +0300

    Add project files.

---
+++ Business/Abstract/IApplicantService.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IApplicantService
+    {
+        //Birden çok yapıda ortak fonksiyonlar gerektiğinde temiz kod için interfacede-
+        //fonksiyonları barındırırız.
+
+        //If a lot of structure require same functions,we can use interface for clean code.
+        void ApplyForMask(Person person);
+
+        List<Person> GetList();
+
+         bool CheckPerson(Person person);
+       
+    }
+}
---
+++ WorkAround/Program.cs
@@ -0,0 +1,52 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace WorkAround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vatandas vatandas1 = new Vatandas();
+
+            List<string> sehirler = new List<string> { "Ankara", "İstanbul", "İzmir" };
+            sehirler.Add("Isparta");
+
+            foreach (string sehir in sehirler)
+            {
+                //Console.WriteLine(sehir);
+            }
+            Person person1 = new Person();
+            person1.FirstName = "SAMİ";
+            person1.LastName = "KÖSE";
+            person1.DateOfBirthYear = 2002;
+            person1.NationalIdentity = 37018039180;
+
+            Person person2 = new Person();
+            person2.FirstName = "Murat";
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.giveMask(person1);
+
+        }
+
+        static void SelamVer()
+        {
+            Console.WriteLine("Merhaba");
+        }
+
+
+    }
+
+    
+
+    public class Vatandas
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int DogumYili { get; set; }
+        public long TcNo { get; set; }
+    }
+}
